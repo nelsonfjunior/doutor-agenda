de6b767c03b3da20f1c4dfa32c423ee605d34da5
nelsonfjunior <ferreiranelson123@hotmail.com>
2025-05-27 22:01:29 -0300

    feat: implement session management and sign out functionality

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,25 @@
-const DashboardPage = () => {
-    return ( 
-        <div>Dashboard</div>
-     );
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+
+import SignOutButton from "./components/signOutButton";
+
+const DashboardPage = async () => {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+    if (!session?.user) {
+        redirect("/authentication");
+    }
+
+    return (
+        <div>
+            <h1>{session?.user.email}</h1>
+            <h1>{session?.user.name}</h1>
+            <SignOutButton />
+        </div>
+    );
 }
- 
+
 export default DashboardPage;
