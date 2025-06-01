64e7fe2976e29d9b631f9a4b35584c364b6698b1
nelsonfjunior <ferreiranelson123@hotmail.com>
2025-06-01 00:44:09 -0300

    feat: add availability checks for appointment times in form

--- src/app/(protected)/appointments/_components/add-appointment-form.tsx
+++ src/app/(protected)/appointments/_components/add-appointment-form.tsx
@@ -100,7 +100,8 @@ const AddAppointmentForm = ({
     queryFn: () => getAvailableTimes({
       date: dayjs(selectedDate).format("YYYY-MM-DD"),
       doctorId: selectedDoctorId,
-    })
+    }),
+    enabled: !!selectedDate && !!selectedDoctorId,
   })
 
   // Atualizar o preço quando o médico for selecionado
@@ -148,7 +149,7 @@ const AddAppointmentForm = ({
   };
 
   const isDateAvailable = (date: Date) => {
-    if (!selectedDoctorId || !availableTimes) return false;
+    if (!selectedDoctorId) return false;
     const selectedDoctor = doctors.find(
       (doctor) => doctor.id === selectedDoctorId,
     );
